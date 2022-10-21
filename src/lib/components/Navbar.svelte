178362c20586cd3ff51453cea06065510e45c2a9
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-21 04:23:54 -0300

    Added responsive

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,20 +2,23 @@
 	import Button from '$lib/components/Button.svelte';
 </script>
 
-<nav class="navbar">
-	<a class="col-start-1 lg:col-start-2" href="/">
-		<img
-			class="cursor-pointer"
-			src="/corp_logo.webp"
-			width="350"
-			alt="Corporación Municipal de Deportes Quintero"
-		/>
+<nav>
+	<a href="/">
+		<img src="/corp_logo.webp" width="350" alt="Corporación Municipal de Deportes Quintero" />
 	</a>
 	<Button class="justify-self-end lg:justify-self-center" to="/login" text="Iniciar Sesión" />
 </nav>
 
 <style scoped>
-	.navbar {
-		@apply w-full inline-grid grid-cols-2 lg:grid-cols-3 place-items-center gap-4 px-14 lg:px-0 bg-lightsecondary text-darkblue shadow-md;
+	nav {
+		@apply w-full min-w-[375px] inline-grid grid-cols-2 lg:grid-cols-3 place-items-center gap-4 px-4 lg:px-14 py-2 bg-lightsecondary text-darkblue shadow-md;
+	}
+
+	nav a {
+		@apply col-start-1 lg:col-start-2;
+	}
+
+	a img {
+		@apply cursor-pointer min-w-[180px];
 	}
 </style>
