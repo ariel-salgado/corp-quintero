d559ffd171c62cfa9f9d8f3a596ac45bc99d3512
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-21 02:08:32 -0300

    Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let text: string;
+	export let to: string | undefined = undefined;
+	export let action: () => void | undefined = () => {};
+</script>
+
+<a class={`${$$restProps.class} btn`} href={to} on:click={action}>{text}</a>
+
+<style scoped>
+	.btn {
+		@apply min-w-max bg-darkblue hover:bg-lightblue text-lightsecondary text-lg rounded-lg py-3 px-4 cursor-pointer transition-all duration-300;
+	}
+</style>
