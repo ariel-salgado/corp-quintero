3d0e802c0c7b4bec7c1006e5f735921304c75f23
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-21 04:23:40 -0300

    Handled undefined $$restProps

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -4,10 +4,10 @@
 	export let action: () => void | undefined = () => {};
 </script>
 
-<a class={`${$$restProps.class} btn`} href={to} on:click={action}>{text}</a>
+<a class={`${$$restProps.class ?? $$restProps.class} btn`} href={to} on:click={action}>{text}</a>
 
 <style scoped>
 	.btn {
-		@apply min-w-max bg-darkblue hover:bg-lightblue text-lightsecondary text-sm font-bold uppercase rounded-lg py-3 px-4 cursor-pointer transition-all duration-300;
+		@apply min-w-max bg-darkblue hover:bg-lightblue text-lightsecondary text-xs lg:text-sm font-bold uppercase rounded-lg py-3 px-4 cursor-pointer transition-all duration-300;
 	}
 </style>
