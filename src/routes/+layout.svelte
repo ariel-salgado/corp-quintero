2d0d839dade4f31061d09ded20816f9bbfa3ad7c
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-21 05:48:53 -0300

    Added Footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
 	import '../app.css';
 	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <Navbar />
-<slot />
+<main class="h-screen">
+	<slot />
+</main>
+<Footer />
