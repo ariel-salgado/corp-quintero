5c14ff92af7e3a9440970d8baba9567c1d8d7589
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-21 02:07:31 -0300

    Added custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,29 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				lightprimary: 'rgb(var(--light-primary) / <alpha-value>)',
+				lightsecondary: 'rgb(var(--light-secondary) / <alpha-value>)',
+				darkprimary: 'rgb(var(--dark-primary) / <alpha-value>)',
+				darksecondary: 'rgb(var(--dark-secondary) / <alpha-value>)',
+				neutralorange: 'rgb(var(--orange-neutral) / <alpha-value>)',
+				lightorange: 'rgb(var(--orange-light) / <alpha-value>)',
+				darkorange: 'rgb(var(--orange-dark;) / <alpha-value>)',
+				neutralgreen: 'rgb(var(--green-neutral) / <alpha-value>)',
+				lightgreen: 'rgb(var(--green-light) / <alpha-value>)',
+				darkgreen: 'rgb(var(--green-dark;) / <alpha-value>)',
+				neutralyellow: 'rgb(var(--yellow-neutral) / <alpha-value>)',
+				lightyellow: 'rgb(var(--yellow-light) / <alpha-value>)',
+				darkyellow: 'rgb(var(--yellow-dark) / <alpha-value>)',
+				neutralblue: 'rgb(var(--blue-neutral) / <alpha-value>)',
+				lightblue: 'rgb(var(--blue-light) / <alpha-value>)',
+				darkblue: 'rgb(var(--blue-dark) / <alpha-value>)'
+			},
+			fontFamily: {
+				josefin: "'Josefin Sans', sans-serif"
+			}
+		}
 	},
 	plugins: []
 };
