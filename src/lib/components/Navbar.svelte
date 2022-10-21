906c7c81590093c62df92562ab21d91c2b83dc20
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-21 02:09:26 -0300

    Navbar component

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Button from '$lib/components/Button.svelte';
+</script>
+
+<nav class="navbar">
+	<a class="col-start-1 lg:col-start-2" href="/">
+		<img
+			class="cursor-pointer"
+			src="/corp_logo.webp"
+			width="350"
+			alt="Corporación Municipal de Deportes Quintero"
+		/>
+	</a>
+	<Button to="/login" text="Iniciar Sesión" />
+</nav>
+
+<style scoped>
+	.navbar {
+		@apply w-full inline-grid grid-cols-2 lg:grid-cols-3 place-items-center gap-4 bg-lightsecondary text-darkblue shadow-md;
+	}
+</style>
