aa8fbef1865410037ce8a820f8374cf333659765
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-21 02:10:20 -0300

    Added head title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Corporación Municipal de Deportes Quintero</title>
+</svelte:head>
