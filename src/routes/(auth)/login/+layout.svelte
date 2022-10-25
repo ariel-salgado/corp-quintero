be3fa52d2af4449ec0a54509170d4e24dc2b11a3
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-25 04:06:00 -0300

    Login page

--- src/routes/(auth)/login/+layout.svelte
+++ src/routes/(auth)/login/+layout.svelte
@@ -1,3 +1,9 @@
 <main>
 	<slot />
 </main>
+
+<style scoped>
+	main {
+		@apply h-screen grid grid-flow-col place-items-center text-center grid-cols-1 lg:grid-cols-2;
+	}
+</style>
--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,3 +1,38 @@
+<script lang="ts">
+	import Button from '$lib/components/Button.svelte';
+	import Input from '$lib/components/Input.svelte';
+
+	function login() {
+		alert('login');
+	}
+</script>
+
 <svelte:head>
 	<title>Ingreso | Corporación Municipal de Deportes Quintero</title>
 </svelte:head>
+
+<aside />
+<section>
+	<a href="/">
+		<img src="corp_logo.webp" width="350" alt="Corporación Municipal de Deportes Quintero" />
+	</a>
+	<form>
+		<Input label="Usuario" ph="Ingrese Usuario" />
+		<Input label="Contraseña" ph="Ingrese Contraseña" />
+		<Button text="Ingresar" action={login} />
+	</form>
+</section>
+
+<style scoped>
+	aside {
+		@apply hidden lg:block bg-darkblue w-full h-screen;
+	}
+
+	section {
+		@apply w-full grid place-items-center gap-10 p-16;
+	}
+
+	section form {
+		@apply w-full grid place-items-center text-center gap-8;
+	}
+</style>
