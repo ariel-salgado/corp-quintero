a359751dc4ee02f52ef7fecf528aa80b15c292de
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-25 04:04:56 -0300

    Fixed width

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -8,6 +8,6 @@
 
 <style scoped>
 	a {
-		@apply min-w-max bg-darkblue hover:bg-lightblue text-lightsecondary text-xs lg:text-sm font-bold uppercase rounded-lg py-3 px-4 cursor-pointer transition-all duration-300;
+		@apply w-full min-w-max bg-darkblue hover:bg-lightblue text-lightsecondary text-xs lg:text-sm font-bold uppercase rounded-lg py-3 px-4 cursor-pointer transition-all duration-300;
 	}
 </style>
