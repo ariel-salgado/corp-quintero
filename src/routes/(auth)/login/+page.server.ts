e0719804a8f6782b46fc1dbd3890afdd560a041a
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-25 13:11:07 -0300

    Basic form validation

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -1,10 +1,36 @@
 export const prerender = false;
 
+import { z, ZodError } from 'zod';
 import type { Actions } from './$types';
 
+const LoginSchema = z.object({
+	username: z
+		.string({ required_error: 'El usuario es obligatorio' })
+		.min(1, { message: 'El usuario es obligatorio' })
+		.max(50, { message: 'El usuario debe ser menor a 50 caracteres' })
+		.trim(),
+	password: z
+		.string({ required_error: 'La contraseña es obligatoria' })
+		.min(6, { message: 'La contraseña debe ser mayor a 6 caracteres' })
+		.max(50, { message: 'La contraseña debe ser menor a 50 caracteres' })
+		.trim()
+});
+
 export const actions: Actions = {
-  default: async ({ request }) => {
-    const formData = Object.fromEntries(await request.formData());
-		console.log(formData);
-  }
-}
+	default: async ({ request }) => {
+		const formData = Object.fromEntries(await request.formData());
+
+		try {
+			const result = LoginSchema.parse(formData);
+		} catch (err: unknown) {
+			if (err instanceof ZodError) {
+				const { fieldErrors: errors } = err.flatten();
+				const { password, ...rest } = formData;
+				return {
+					data: rest,
+					errors
+				};
+			}
+		}
+	}
+};
