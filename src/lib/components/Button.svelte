05d108101a11109d378d734fcf8c25d3f0d5abc6
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-25 04:24:17 -0300

    Fixed double event

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+
 	export let text: string;
 	export let to: string | undefined = undefined;
 	export let action: () => void | undefined = () => {};
+
+	function handleClick() {
+		if (to) {
+			goto(to);
+		} else if (action) {
+			action();
+		}
+	}
 </script>
 
-<a href={to} on:click={action}>{text}</a>
+<button on:click={handleClick}>{text}</button>
 
 <style scoped>
-	a {
+	button {
 		@apply w-full min-w-max bg-darkblue hover:bg-lightblue text-lightsecondary text-xs lg:text-sm font-bold uppercase rounded-lg py-3 px-4 cursor-pointer transition-all duration-300;
 	}
 </style>
