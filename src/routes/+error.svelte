b98a6b1ec88c7ba23e59c9b7848220e09952d35a
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-25 02:13:01 -0300

    Error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<section>
+	<h1>Página no encontrada</h1>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+		laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+		voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+		non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</p>
+</section>
+
+<style scoped>
+	section {
+		@apply h-screen grid place-items-center text-center text-darkblue bg-lightsecondary m-8 p-36 rounded-lg shadow-md;
+	}
+
+	h1 {
+		@apply font-bold text-6xl;
+	}
+</style>
