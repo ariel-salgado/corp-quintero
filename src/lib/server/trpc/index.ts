09ed73acdf0e44fd8df4b00ea47db154d73e81fc
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-27 04:24:08 -0300

    Rename router

--- src/lib/server/trpc/index.ts
+++ src/lib/server/trpc/index.ts
@@ -1,6 +1,6 @@
 import { createRouter } from '$lib/server/createRouter';
 import { login } from '$lib/server/trpc/login';
 
-export const apiRouter = createRouter().merge('login', login);
+export const router = createRouter().merge('login', login);
 
-export type ApiRouter = typeof apiRouter;
+export type ApiRouter = typeof router;
