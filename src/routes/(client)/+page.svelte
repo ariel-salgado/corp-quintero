a9a237fbdc96a173321a2d51ba220bd2466aa1b4
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-27 04:23:50 -0300

    Api call example

--- src/routes/(client)/+page.svelte
+++ src/routes/(client)/+page.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
+	import trpc from '$lib/trpc';
+
+	const load = async () => {
+		const message = await trpc().query('login', { nombre: 'foo', contrasena: 'bar' });
+		return message;
+	};
 </script>
 
 <svelte:head>
 	<title>Corporación Municipal de Deportes Quintero</title>
 </svelte:head>
+
+{#await load()}
+	Loading...
+{:then value}
+	{value}
+{:catch error}
+	{error}
+{/await}
