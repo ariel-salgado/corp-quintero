7e7f6fb35d3ee17d20c992a9affd4daa9bc99663
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-27 03:11:13 -0300

    Added login schema validator

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -1,20 +1,8 @@
 export const prerender = false;
 
-import { z, ZodError } from 'zod';
 import type { Actions } from './$types';
-
-const LoginSchema = z.object({
-	username: z
-		.string({ required_error: 'El usuario es obligatorio' })
-		.min(1, { message: 'El usuario es obligatorio' })
-		.max(50, { message: 'El usuario debe ser menor a 50 caracteres' })
-		.trim(),
-	password: z
-		.string({ required_error: 'La contraseña es obligatoria' })
-		.min(6, { message: 'La contraseña debe ser mayor a 6 caracteres' })
-		.max(50, { message: 'La contraseña debe ser menor a 50 caracteres' })
-		.trim()
-});
+import { LoginSchema } from 'src/utils/zodValidations';
+import { ZodError } from 'zod';
 
 export const actions: Actions = {
 	default: async ({ request }) => {
