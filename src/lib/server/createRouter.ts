d03d81706aaf8198e4d86ead43ed4fb445504850
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-27 03:12:10 -0300

    Create router function

---
+++ src/lib/server/createRouter.ts
@@ -0,0 +1,10 @@
+import type { inferAsyncReturnType } from '@trpc/server';
+import * as trpc from '@trpc/server';
+
+export async function createContext() {}
+
+export type Context = trpc.inferAsyncReturnType<typeof createContext>;
+
+export function createRouter() {
+	return trpc.router<Context>();
+}
