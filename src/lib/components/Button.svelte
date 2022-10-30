57713b8bd95ee995a566e2724142a3db397ba611
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-30 04:10:20 -0300

    Added prefetching

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,23 +1,18 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-
 	export let text: string;
 	export let to: string | undefined = undefined;
 	export let action: () => void | undefined = () => {};
-
-	function handleClick() {
-		if (to) {
-			goto(to);
-		} else if (action) {
-			action();
-		}
-	}
 </script>
 
-<button on:click={handleClick}>{text}</button>
+{#if to}
+	<a href={to}>{text}</a>
+{:else}
+	<button on:click={action}>{text}</button>
+{/if}
 
 <style scoped>
-	button {
+	button,
+	a {
 		@apply w-full min-w-max bg-darkblue hover:bg-lightblue text-lightsecondary text-xs lg:text-sm font-bold uppercase rounded-lg py-3 px-4 cursor-pointer transition-all duration-300;
 	}
 </style>
