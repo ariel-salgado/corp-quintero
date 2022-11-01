b524e4148a5d13620a13ea6d4a21675e4c24f40f
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-11-01 20:32:56 -0300

    Change exports names

--- src/utils/zod/schema.ts
+++ src/utils/zod/schema.ts
@@ -7,12 +7,12 @@ import {
 	evento_estado
 } from '@prisma/client';
 
-export const persona = z.object({
+export const personaZod = z.object({
 	rut: z.string(),
 	nombres: z.string(),
 	apellidos: z.string(),
 	telefono_personal: z.number().int(),
-	telefono_contacto: z.number().int().nullish(),
+	telefono_contacto: z.number().int(),
 	correo: z.string(),
 	fecha_nacimiento: z.date(),
 	direccion: z.string(),
@@ -20,14 +20,14 @@ export const persona = z.object({
 	talla: z.nativeEnum(persona_talla)
 });
 
-export const usuario = z.object({
+export const usuarioZod = z.object({
 	rut: z.string(),
 	nombre: z.string(),
 	contrasena: z.string(),
 	estado: z.nativeEnum(usuario_estado)
 });
 
-export const evento = z.object({
+export const eventoZod = z.object({
 	id: z.number().int(),
 	nombre: z.string(),
 	tipo: z.nativeEnum(evento_tipo),
@@ -42,12 +42,12 @@ export const evento = z.object({
 	estado: z.nativeEnum(evento_estado)
 });
 
-export const categoria_evento = z.object({
+export const categoria_eventoZod = z.object({
 	id_evento: z.number().int(),
 	categoria: z.string()
 });
 
-export const inscribe_evento = z.object({
+export const inscribe_eventoZod = z.object({
 	rut: z.string(),
 	id_evento: z.number().int(),
 	fecha: z.date(),
