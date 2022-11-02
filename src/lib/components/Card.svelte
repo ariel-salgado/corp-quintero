05bc12ac176800fa4370633222db71bd96041e65
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-11-02 19:20:05 -0300

    Evento card

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import type { evento } from '@prisma/client';
+	import Button from '$lib/components/Button.svelte';
+
+	export let evento: evento;
+</script>
+
+<article>
+	<img src={`/eventos/${evento.id}.webp`} alt={evento.nombre} />
+	<div>
+		<h1>{evento.nombre}</h1>
+		<p>
+			{evento.descripcion}
+		</p>
+		<Button to={`evento/${evento.id}`} text="Ver más" />
+	</div>
+</article>
+
+<style scoped>
+	article {
+		@apply w-full min-w-[300px] flex flex-col md:flex-row bg-lightsecondary rounded-lg hover:scale-105 duration-300 shadow-lg hover:shadow-xl cursor-pointer;
+	}
+
+	img {
+		@apply w-full md:w-1/2 object-cover rounded-t-lg md:rounded-t-none md:rounded-l-lg;
+	}
+
+	div {
+		@apply w-full flex flex-col justify-between p-6 md:p-12;
+	}
+
+	h1 {
+		@apply text-lg md:text-3xl font-medium;
+	}
+
+	p {
+		@apply text-sm md:text-base py-4;
+	}
+</style>
