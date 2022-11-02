4d4849bcc8c33e8bc01cdf3c201ae376e5a0cd48
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-11-02 15:28:43 -0300

    Style scoped

--- src/lib/components/Eventos.svelte
+++ src/lib/components/Eventos.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import type { evento } from '@prisma/client';
 	import Card from '$lib/components/Card.svelte';
+
 	export let eventos: evento[];
 </script>
 
-<section class="grid py-12 px-16 gap-y-12">
+<section>
 	{#each eventos as evento}
 		<Card {evento} />
 	{/each}
 </section>
+
+<style scoped>
+	section {
+		@apply grid py-6 lg:py-12 px-6 lg:px-16 gap-y-8 lg:gap-y-12;
+	}
+</style>
