76c56efc5526d607bc4e74e60c8f0c07f6b06e57
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-11-02 03:56:25 -0300

    Eventos layout

---
+++ src/lib/components/Eventos.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { evento } from '@prisma/client';
+	import Card from '$lib/components/Card.svelte';
+	export let eventos: evento[];
+</script>
+
+<section class="grid py-12 px-16 gap-y-12">
+	{#each eventos as evento}
+		<Card {evento} />
+	{/each}
+</section>
