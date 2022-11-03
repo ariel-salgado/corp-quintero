ec2841a209fa88a67b6ed652be496f7600818ce6
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-11-03 19:48:56 -0300

    Added styles and responsive

--- src/routes/(client)/inscripcion/[slug]/+page.svelte
+++ src/routes/(client)/inscripcion/[slug]/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { goto } from '$app/navigation';
+	import { persona_sexo } from '@prisma/client';
 	import Input from '$src/lib/components/Form/Input.svelte';
 	import Select from '$src/lib/components/Form/Select.svelte';
 	import Date from '$src/lib/components/Form/Date.svelte';
-	import { persona_sexo } from '@prisma/client';
+	import Button from '$lib/components/Button.svelte';
 
 	export let data: PageData;
 	const { categorias } = data;
@@ -13,34 +14,97 @@
 </script>
 
 <svelte:head>
-	<title>Inscripción | Corporación Municipal de Deportes Quintero</title>
+	<title>Inscripción {categorias.nombre} | Corporación Municipal de Deportes Quintero</title>
 </svelte:head>
 
-<form method="post">
-	<Select
-		label="Categoría"
-		name="categoria"
-		ph="Selecciona una categoría"
-		options={categorias.categoria_evento.map((evento) => {
-			return evento.categoria;
-		})}
-	/>
+<section>
+	<form method="post">
+		<h1>{`Inscripción ${categorias.nombre}`}</h1>
 
-	<Input label="RUT" name="rut" ph="Ingrese su RUT" />
+		<div>
+			<Select
+				label="Categoría"
+				name="categoria"
+				ph="Selecciona una categoría"
+				options={categorias.categoria_evento.map((evento) => {
+					return evento.categoria;
+				})}
+			/>
+		</div>
 
-	<Input label="Nombres" name="nombres" ph="Ingrese sus nombres" />
+		<div>
+			<Input label="RUT" name="rut" ph="Ingrese su RUT" />
+		</div>
 
-	<Input label="Apellidos" name="apellidos" ph="Ingrese sus apellidos" />
+		<div>
+			<Input label="Nombres" name="nombres" ph="Ingrese sus nombres" />
+		</div>
 
-	<Select label="Sexo" name="sexo" ph="Seleccione su sexo" options={Object.values(persona_sexo)} />
+		<div>
+			<Input label="Apellidos" name="apellidos" ph="Ingrese sus apellidos" />
+		</div>
 
-	<Date label="Fecha de nacimiento" name="fecha_nacimiento" ph="Ingrese su fecha de nacimiento" />
+		<div>
+			<Select
+				label="Sexo"
+				name="sexo"
+				ph="Seleccione su sexo"
+				options={Object.values(persona_sexo)}
+			/>
+		</div>
 
-	<Input label="Teléfono personal" name="telefono_personal" ph="Ingrese su teléfono de personal" />
+		<div>
+			<Date
+				label="Fecha de nacimiento"
+				name="fecha_nacimiento"
+				ph="Ingrese su fecha de nacimiento"
+			/>
+		</div>
 
-	<Input label="Teléfono contacto" name="telefono_contacto" ph="Ingrese su teléfono de contacto" />
+		<div>
+			<Input
+				label="Teléfono personal"
+				name="telefono_personal"
+				ph="Ingrese su teléfono de personal"
+			/>
+		</div>
 
-	<Input label="Correo electrónico" name="correo" ph="Ingrese su correo electrónico" />
+		<div>
+			<Input
+				label="Teléfono contacto"
+				name="telefono_contacto"
+				ph="Ingrese su teléfono de contacto"
+			/>
+		</div>
 
-	<Input label="Dirección" name="direccion" ph="Ingrese su dirección" />
-</form>
+		<div>
+			<Input label="Correo electrónico" name="correo" ph="Ingrese su correo electrónico" />
+		</div>
+
+		<div>
+			<Input label="Dirección" name="direccion" ph="Ingrese su dirección" />
+		</div>
+
+		<div>
+			<Button text="Volver" to={`/evento/${categorias.id}`} />
+			<Button text="Inscribirse" />
+		</div>
+	</form>
+</section>
+
+<style scoped>
+	section {
+		@apply flex justify-center p-6 lg:py-12;
+	}
+	form {
+		@apply w-full max-w-3xl flex flex-col gap-y-3 bg-lightsecondary rounded-lg shadow-lg p-6;
+	}
+
+	form div:last-child {
+		@apply flex flex-col-reverse gap-y-4 md:flex-row md:gap-x-4 mt-4;
+	}
+
+	h1 {
+		@apply text-2xl font-bold;
+	}
+</style>
