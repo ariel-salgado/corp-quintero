6c4341c6c3dbcbc7948e40fa7e4a772862d65be9
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-11-03 05:49:33 -0300

    Fetching evento categories

---
+++ src/routes/(client)/inscripcion/[slug]/+page.ts
@@ -0,0 +1,15 @@
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+import trpc from '$lib/trpc';
+
+export const load: PageLoad = async ({ params, fetch }) => {
+	if (!Number(params.slug)) throw error(404, 'Not Found');
+
+	const categorias = await trpc(fetch).query('eventos:category', { id: Number(params.slug) });
+
+	if (!categorias) throw error(404, 'Not Found');
+
+	return {
+		categorias: categorias
+	};
+};
