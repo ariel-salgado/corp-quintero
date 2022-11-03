62548dd636f6a739c82084c0b49b8885c708f349
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-11-02 23:23:17 -0300

    Showing eventos categories

---
+++ src/lib/components/Badges.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let categorias: { categoria: string }[] | undefined;
+</script>
+
+{#if categorias}
+	<div>
+		{#each categorias as evento}
+			<span>{evento.categoria}</span>
+		{/each}
+	</div>
+{/if}
+
+<style scoped>
+	div {
+		@apply flex flex-row gap-x-4;
+	}
+
+	span {
+		@apply bg-darkgreen p-2 text-lightsecondary rounded-lg text-xs lg:text-sm font-bold uppercase;
+	}
+</style>
