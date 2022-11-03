42a5d13e4e995a27d23a78687773069cd16bede4
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-11-03 02:48:02 -0300

    Added error handling

--- src/routes/(client)/+page.ts
+++ src/routes/(client)/+page.ts
@@ -1,8 +1,12 @@
+import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 import trpc from '$lib/trpc';
 
 export const load: PageLoad = async ({ fetch }) => {
 	const eventos = await trpc(fetch).query('eventos:all-actives');
+
+	if (!eventos) throw error(404, 'Not Found');
+
 	return {
 		eventos: eventos
 	};
