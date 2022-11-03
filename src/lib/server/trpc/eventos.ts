fdd7b12018733c2b073f2f2303bf34558625e941
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-11-02 23:24:25 -0300

    Fetching only necessary data

--- src/lib/server/trpc/eventos.ts
+++ src/lib/server/trpc/eventos.ts
@@ -2,8 +2,48 @@ import { createRouter } from '$lib/server/createRouter';
 import prismaClient from '$lib/server/prismaClient';
 import { z } from 'zod';
 
-export const eventos = createRouter().query(':all', {
-	resolve: async () => {
-		return prismaClient.evento.findMany();
-	}
-});
+export const eventos = createRouter()
+	.query(':all-actives', {
+		resolve: async () => {
+			return prismaClient.evento.findMany({
+				where: {
+					estado: 'Activo'
+				},
+				select: {
+					id: true,
+					nombre: true,
+					descripcion: true
+				}
+			});
+		}
+	})
+	.query(':one', {
+		input: z.object({
+			id: z.number()
+		}),
+		resolve: async ({ input }) => {
+			return prismaClient.evento.findUnique({
+				where: {
+					id: input.id
+				},
+				select: {
+					id: true,
+					tipo: true,
+					nombre: true,
+					fecha_termino: true,
+					hora_inicio: true,
+					hora_termino: true,
+					cupo: true,
+					direccion: true,
+					descripcion: true,
+					requisitos: true,
+					estado: true,
+					categoria_evento: {
+						select: {
+							categoria: true
+						}
+					}
+				}
+			});
+		}
+	});
