c8a7d0e67b4a71a6bc5ff3e475f227cc7eae5b9f
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-11-18 20:51:59 -0300

    Added dashboard landpage

--- src/routes/(admin)/dashboard/+page.svelte
+++ src/routes/(admin)/dashboard/+page.svelte
@@ -1,1 +1,18 @@
-<h1>Hello Admin!</h1>
+<script lang="ts">
+	import Table from '$lib/components/dashboard/Table.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	const { eventos } = data;
+
+	const cols: string[] = ['Nombre', 'Fecha', 'Direccion', 'Cupos', 'Disponibles', 'Opciones'];
+</script>
+
+<section>
+	<Table
+		tableTitle="Eventos Activos"
+		tableDescription="Eventos deportivos que se encuentran disponibles en la página principal de la corporación"
+		{cols}
+		rows={eventos}
+	/>
+</section>
---
+++ src/routes/(admin)/dashboard/+page.ts
@@ -0,0 +1,13 @@
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+import trpc from '$lib/trpc';
+
+export const load: PageLoad = async ({ fetch }) => {
+	const eventos = await trpc(fetch).query('dashboard:actives');
+
+	if (!eventos) throw error(404, 'Not Found');
+
+	return {
+		eventos: eventos
+	};
+};
