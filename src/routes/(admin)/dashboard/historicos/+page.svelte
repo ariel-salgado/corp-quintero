7f77b18b086d6147d0d59e385cb6087c26be141a
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-11-18 20:51:06 -0300

    Added eventos historicos

---
+++ src/routes/(admin)/dashboard/historicos/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Table from '$lib/components/dashboard/Table.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	const { eventos } = data;
+
+	const cols: string[] = ['Nombre', 'Fecha', 'Direccion', 'Cupos', 'Disponibles', 'Opciones'];
+</script>
+
+<section>
+	<Table
+		tableTitle="Eventos Historicos"
+		tableDescription="Lista completa de los eventos deportivos ofrecidos por la corporación, incluyendo los eventos que ya han finalizado"
+		{cols}
+		rows={eventos}
+	/>
+</section>
---
+++ src/routes/(admin)/dashboard/historicos/+page.ts
@@ -0,0 +1,13 @@
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+import trpc from '$lib/trpc';
+
+export const load: PageLoad = async ({ fetch }) => {
+	const eventos = await trpc(fetch).query('dashboard:all');
+
+	if (!eventos) throw error(404, 'Not Found');
+
+	return {
+		eventos: eventos
+	};
+};
