4e9f02e0c37337b22d413e69984728bef5677434
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-11-18 20:53:11 -0300

    Added dashboard layout

--- src/routes/(admin)/dashboard/+layout.svelte
+++ src/routes/(admin)/dashboard/+layout.svelte
@@ -1,1 +1,14 @@
-<slot />
+<script lang="ts">
+	import Navbar from '$src/lib/components/dashboard/Navbar.svelte';
+</script>
+
+<Navbar />
+<main>
+	<slot />
+</main>
+
+<style scoped>
+	main {
+		@apply h-screen min-w-full p-6;
+	}
+</style>
