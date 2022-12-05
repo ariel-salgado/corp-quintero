dfada5f554d79291288968a47003427ee3802caf
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-12-04 22:07:25 -0300

    Added TextArea

---
+++ src/lib/components/Form/TextArea.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let label: string;
+	export let name: string | undefined = undefined;
+	export let ph: string | undefined = undefined;
+	export let value: string | null = null;
+</script>
+
+<!-- svelte-ignore a11y-label-has-associated-control -->
+<div>
+	<label>{label}</label>
+	<textarea on:input {name} placeholder={ph} bind:value cols="5" />
+</div>
+
+<style scoped>
+	div {
+		@apply w-full grid text-left;
+	}
+
+	label {
+		@apply text-sm font-medium text-gray-700 mb-1;
+	}
+
+	textarea {
+		@apply h-20 bg-lightsecondary text-darksecondary text-xs lg:text-sm rounded-lg border border-darkblue focus:outline-none p-2;
+	}
+</style>
