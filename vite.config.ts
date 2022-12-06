ec8120cadc6bf42ebb30238301f826b44e098df3
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-12-06 02:47:10 -0300

    Added vite missing file for production

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'.prisma/client/index-browser': './node_modules/.prisma/client/index-browser.js'
+		}
+	}
 };
 
 export default config;
