e547a44f2c0aae595f974da28ad499c4bd7971d5
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-12-11 00:19:57 -0300

    Prevent multiple instances running at the same time

--- src/lib/server/prismaClient.ts
+++ src/lib/server/prismaClient.ts
@@ -1,3 +1,17 @@
 import { PrismaClient } from '@prisma/client';
-const prismaClient = new PrismaClient();
+
+let prismaClient: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+	prismaClient = new PrismaClient();
+} else {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	if (!(global as any).prismaClient) {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		(global as any).prismaClient = new PrismaClient();
+	}
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	prismaClient = (global as any).prismaClient;
+}
+
 export default prismaClient;
